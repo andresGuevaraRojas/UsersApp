ed387df12e4288d1c14662026653870ae2a18be9
Andres Guevara <andy_jose_andres@hotmail.com>
2023-09-02 16:05:52 -0600

    Install react native config to handle env files

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigator from './src/navigation/RootStackNavigator';
+import Config from 'react-native-config';
+
+console.log(Config.API_URL);
 
 export default function App() {
   return (
